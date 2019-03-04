e9c144d0472ab349c90c8e649911f796ba3e562e
Michael Kafarowski <michael.kafarowski@gmail.com>
2019-03-03 23:56:29 -0500

    Add server code and readme

--- README.md
+++ README.md
@@ -3,6 +3,16 @@
 The Unity Module is an I2C-based modular control board for the GranolaPods smart trail-mix making machine.
 This was developed for University of Waterloo Mechatronics FYDP team #26.
 
-This repo contains the PCB and code.
+Why the name "Unity"? The board and code is designed to be modular - all devices in the GranolaPods system (e.g. the Gantry Cart, the Dispensers, and any future devices such as mixers or binders) use the same PCB and the same code.
 
-For a demonstration of the system you can check out [this video](https://www.youtube.com/watch?v=NxC_GapGs20).
+This repo contains the PCB and code for the Unity modules, as well as major updates to the frontend web interface and the backend server.
+The front end and server are both run locally on a Raspberry Pi. In the GranolaPods system we are using a 7" touchscreen that will display the user interface via a web browser. Submitting a request will generate an HTTP POST, and pass values to the python backend. The backend running on the Pi will sequentially coordinate the Unity modules down the chain to create the snack. Communication between the Pi master and n Unity slaves are done with i2c.
+
+The Unity modules are powered by an ATmega328P. Programming is done through the Arduino IDE via ICSP pins.
+
+To see how the GranolaPods system was envisioned to work check out [this video](https://www.youtube.com/watch?v=Usi5u81KuWI).
+For a demonstration of the Unity system you can check out [this video](https://www.youtube.com/watch?v=NxC_GapGs20).
+
+To run the server, simply run ```python ./server/server.py```, then navigate to ```localhost```.
+
+Currently the system will crash if a GranolaPod is requested with no Unity boards attached. It would probably crash even more spectacularly if you tried running it on something other than a Raspberry Pi.
---
+++ Server/granolapods.py
@@ -0,0 +1,94 @@
+#/'/
+
+import smbus
+import time
+
+bus = smbus.SMBus(1)
+
+def writeNumber(address, value):
+	bus.write_byte(address, value)
+	return -1
+
+def readNumber(address):
+	number = bus.read_byte(address)
+	return number
+
+def readData(address):
+	time.sleep(0.01)
+	value =  bus.read_i2c_block_data(address,0)
+	return value
+
+def sendCommand(address, command, p1,p2,p3):
+	#Commands:
+	# Stepper Direction (1 for CCW and 2 for CW). Speed (RPM)
+	# 0x01: Stepper Free Rotate (direction, speed, 0)
+	# 0x02: Stepper Timed Rotate (direction, speed, seconds)
+	# 0x03: Stepper Steps Rotate (direction, speed, steps)
+	# 0x04: Stepper Stop (0,0,0)
+	# 0x05: Calibrate (0,0,0)
+	# 0x10: Request Stepper (0,0,0)
+	# 0x11: Request Hall Sensor (0,0,0)
+	# 0xaa: Wait (command, response)
+	data = [command, p1, p2, p3]
+	print("Sending command: " + str(command) + " to " + str(address) + " with args " + str(p1) + ", " + str(p2) + ", " + str(p3) + ".")
+	try:
+		bus.write_i2c_block_data(address,0,data)
+	except:
+		pass
+	time.sleep(0.01)
+
+def waitOnUnity(address,command,response):
+	#poll for data on that address until event is tripped (or 20 seconds)
+	print("Waiting on Unity at " + str(address) + " for " + str(response) + " response to command " + str(command) + ".")
+	sendCommand(address,0xaa,command,response,0)
+
+	pollCount = 0;
+	data = readData(address)
+
+	while (data[1] is not response) and (pollCount < 20):
+		data = readData(address)
+		pollCount=pollCount+1
+		time.sleep(1)
+	if pollCount is 20:
+		print("[!] i2c read error or Unity process incomplete")
+		return False
+	else:
+		print("Unity process completed successfully.")
+		return True
+
+def newRequestCallback(pval, fval, cval):
+	print("New Request Received:")
+	print("Protein: " +str(pval) + "%")
+	print("Fat: " +str(fval) + "%")
+	print("Carbs: " + str(cval) + "%\n")
+	#New Request Received. Perform the following actions:
+	#sendCommand(0x9,0x1,1,60,0)
+	#time.sleep(1)
+	#sendCommand(0x9,0x4,0,0,0)
+	#Calibrate Gantry Cart
+	sendCommand(0xA,0x1,1,60,0)
+	waitOnUnity(0xA,0x5,1)
+
+	#Move Cart to First Ingredient
+	sendCommand(0xA,0x1,2,150,0)
+	waitOnUnity(0x9,0x11,1)
+	sendCommand(0xA,0x4,0,0,0)
+	time.sleep(1)
+	sendCommand(0xA,0x1,1,20,0)
+	time.sleep(2)
+	sendCommand(0xA,0x4,0,0,0)
+	time.sleep(1)
+
+	#Dispense Ingredient
+	sendCommand(0x9,0x1,1,40,0)
+	time.sleep(2)
+	sendCommand(0x9,0x4,0,0,0)
+	time.sleep(2)
+
+	#Move Cart Away From Ingredient
+	sendCommand(0xA,0x1,2,150,0)
+	time.sleep(3)
+	sendCommand(0xA,0x4,0,0,0)
+
+	print("\nGranolaPod Complete! Returning to UI.\n")
+	time.sleep(3)
